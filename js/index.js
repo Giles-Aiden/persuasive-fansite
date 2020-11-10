bbbf78d04a979319b0a69f458534d634fefcc64f
KillerHawk-Mec <agiles257@west-mec.org>
2020-11-10 03:17:17 -0700

    Added Homepage, Linux page, Dark theme, and cool tux easter egg.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-function darkTheme() {
+function darkThemeIndex() {
 	var body = document.body;
 	var tux = document.getElementById('tux');
 	body.classList.toggle('dark-theme');
-	if (tux.src.slice(tux.src.length - 7, tux.src.length) == 'tux.png') {
+  //Checks if the last 7 letters in the SRC are equal to tux.png, and changes image based on result
+  if (tux.src.slice(tux.src.length - 7, tux.src.length) == 'tux.png') {
 		tux.src = 'images/coolerTux.png';
 	} else {
 		tux.src = 'images/tux.png';
 	}
+}
+
+function darkThemeLinux() {
+	var body = document.body;
+  var items = document.getElementsByClassName('item');
+	body.classList.toggle('dark-theme');
+  for (i = 0; i < items.length; i++) {
+    items[i].classList.toggle('term');
+  }
 }
